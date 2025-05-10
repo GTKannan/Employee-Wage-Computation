c9de3d2c3249d512576a0f025b51bc103c486899
Thamarai kannan <gtkannan2311@gmail.com>
2025-05-10 14:31:07 +0530

    Welcome to EmployeeWage Computation Program on master branch

---
+++ src/com/brlab/employeewage/EmployeeWageComputation.java
@@ -0,0 +1,8 @@
+package com.brlab.employeewage;
+
+public class EmployeeWageComputation {
+
+    public static void main(String[] args) {
+        System.out.println("Welcome to EmployeeWage Computation Program on master branch");
+    }
+}
