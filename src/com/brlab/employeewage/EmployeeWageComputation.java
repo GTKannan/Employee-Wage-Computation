7f0e1a2ed4331e79fd1d746169dc95c6e57ef2b4
GTKannan <gtkannan2311@gmail.com>
2025-05-10 14:47:27 +0530

    [Kannan]Add:Added code to check Employee attendence

--- src/com/brlab/employeewage/EmployeeWageComputation.java
+++ src/com/brlab/employeewage/EmployeeWageComputation.java
@@ -1,8 +1,19 @@
 package com.brlab.employeewage;
 
+import java.util.Scanner;
+
 public class EmployeeWageComputation {
 
     public static void main(String[] args) {
         System.out.println("Welcome to EmployeeWage Computation Program on master branch");
+
+
+        Scanner sc = new Scanner(System.in);
+        System.out.println("enter here\n1 - Present \n2 - Absent");
+        int n = sc.nextInt();
+        if(n == 1)
+            System.out.println("employee is present,....");
+        else
+            System.out.println("employee not present,....");
     }
 }
