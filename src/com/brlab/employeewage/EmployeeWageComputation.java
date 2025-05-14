ecdf298892440421d12f70fcf8d9274a291700e2
GTKannan <gtkannan2311@gmail.com>
2025-05-14 14:33:57 +0530

    [Added] UC6-Code added to calculate wage of employee until some condition

--- src/com/brlab/employeewage/EmployeeWageComputation.java
+++ src/com/brlab/employeewage/EmployeeWageComputation.java
@@ -9,17 +9,20 @@ public class EmployeeWageComputation {
         int wagePerHour = 20;
         int fullTimeHours = 8;
         int partTimeHours = 4;
-        int empHours = 0;
-        int workingDays = 20;
         int totalWage=0;
+        int maxWorkingDays = 20;
+        int maxWorkingHours = 100;
+        int totalHoursWorked=0;
+        int totalDaysWorked=0;
+        int day=1;
 
         System.out.println("Welcome to Employee Wage Computation Program on master branch");
 
         // Create Random object
         Random random = new Random();
-        for (int day = 1; day <= workingDays; day++) {
+        while (totalDaysWorked <= maxWorkingDays && totalHoursWorked <= maxWorkingHours && day <= maxWorkingDays) {
+            int empHours = 0;
             int empStatus = random.nextInt(3);  // 0=Absent, 1=Part-time, 2=Full-time
-
             switch (empStatus) {
                 case 0:
                     System.out.println("Day " + day + ": Absent");
@@ -36,11 +39,21 @@ public class EmployeeWageComputation {
 
             // Calculate daily wage
             int dailyWage = wagePerHour * empHours;
-            System.out.println("Wage for Day " + day + ": ₹" + dailyWage);
             totalWage += dailyWage;
-        }
-        // Display the result
-        System.out.println("\nTotal Monthly Wage: ₹" + totalWage);
 
+            if(empHours!=0){
+                totalDaysWorked++;
+                totalHoursWorked+=empHours;
+            }
+
+            System.out.println("Wage for Day " + day + ": ₹" + dailyWage);
+            System.out.println("Total Hours so far: " + totalHoursWorked + "\n");
+            day++;
+        }
+
+        // Display the result
+        System.out.println("Total Working Days: " + totalDaysWorked);
+        System.out.println("Total Working Hours: " + totalHoursWorked);
+        System.out.println("Total Monthly Wage: ₹" + totalWage);
     }
 }
