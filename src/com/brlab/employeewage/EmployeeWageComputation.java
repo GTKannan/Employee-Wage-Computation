a4351b55cd949e6824a09542211a625c3e7cba3b
GTKannan <gtkannan2311@gmail.com>
2025-05-14 12:02:48 +0530

    [Added] UC5-Code added to calculate wage of employee for month

--- src/com/brlab/employeewage/EmployeeWageComputation.java
+++ src/com/brlab/employeewage/EmployeeWageComputation.java
@@ -10,31 +10,37 @@ public class EmployeeWageComputation {
         int fullTimeHours = 8;
         int partTimeHours = 4;
         int empHours = 0;
+        int workingDays = 20;
+        int totalWage=0;
 
         System.out.println("Welcome to Employee Wage Computation Program on master branch");
 
         // Create Random object
         Random random = new Random();
-        int empStatus = random.nextInt(3);  // 0=Absent, 1=Part-time, 2=Full-time
+        for (int day = 1; day <= workingDays; day++) {
+            int empStatus = random.nextInt(3);  // 0=Absent, 1=Part-time, 2=Full-time
 
-        switch (empStatus) {
-            case 0:
-                System.out.println("Employee is Absent.");
-                break;
-            case 1:
-                System.out.println("Employee is Part-Time.");
-                empHours = partTimeHours;
-                break;
-            case 2:
-                System.out.println("Employee is Full-Time.");
-                empHours = fullTimeHours;
-                break;
+            switch (empStatus) {
+                case 0:
+                    System.out.println("Day " + day + ": Absent");
+                    break;
+                case 1:
+                    System.out.println("Day " + day + ": Part-Time");
+                    empHours = partTimeHours;
+                    break;
+                case 2:
+                    System.out.println("Day " + day + ": Full-Time");
+                    empHours = fullTimeHours;
+                    break;
+            }
+
+            // Calculate daily wage
+            int dailyWage = wagePerHour * empHours;
+            System.out.println("Wage for Day " + day + ": ₹" + dailyWage);
+            totalWage += dailyWage;
         }
-
-        // Calculate daily wage
-        int dailyWage = wagePerHour * empHours;
-
         // Display the result
-        System.out.println("Daily Employee Wage: ₹" + dailyWage);
+        System.out.println("\nTotal Monthly Wage: ₹" + totalWage);
+
     }
 }
